114c3f73b470c2867de3abab8891a3c5f9d6c99c
Paola <paospiniza1@gmail.com>
2025-03-25 18:34:55 -0600

    creacion de objeto Contacto

--- agendaTelefonica/agendaTelefonica/src/org/generation/model/Contacto.java
+++ agendaTelefonica/agendaTelefonica/src/org/generation/model/Contacto.java
@@ -1,4 +1,55 @@
 package org.generation.model;
 
 public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Contacto(String nombre, String apellido, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    // Getters and Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Contacto contacto = (Contacto) obj;
+        return nombre.equalsIgnoreCase(contacto.nombre) && apellido.equalsIgnoreCase(contacto.apellido);
+    }
 }
